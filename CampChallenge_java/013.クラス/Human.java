3f8bc90ee696963bac73615bd06af952cfe8400f
araitsu1262 <brkn15005@g.nihon-u.ac.jp>
2018-05-07 15:02:37 +0900

    課題：クラスの作成

---
+++ CampChallenge_java/013.クラス/Human.java
@@ -0,0 +1,15 @@
+package org.camp.servlet3;
+ 
+
+ public class Human{
+
+    public static void main(String[] args){
+ 
+       Test p = new Test();
+ 
+        p.setTest("a",25);
+        p.hito();
+        
+    }
+
+ }
