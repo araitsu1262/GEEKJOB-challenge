ef92fb43d3f7358d4fac3107a6bad3a5fc378ea9
araitsu1262 <brkn15005@g.nihon-u.ac.jp>
2018-05-21 15:58:11 +0900

    課題：文字数の取得

---
+++ CampChallenge_java/015.標準クラス/mail.java
@@ -0,0 +1,14 @@
+package org.camp.mojiretsu;
+
+
+class mail {
+    
+   public static void main(String[] ages){
+    
+     String address = "arupaka2510@yahoo.co.jp";
+    
+     String a = address.substring(11);
+     System.out.println(a.length());
+    
+   }    
+}
