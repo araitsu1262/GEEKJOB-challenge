650db7219c6733c921c608d793a1cc1dd6131758
araitsu1262 <brkn15005@g.nihon-u.ac.jp>
2018-05-21 15:57:08 +0900

    課題：バイト数と文字数の取得

---
+++ CampChallenge_java/015.標準クラス/name.java
@@ -0,0 +1,17 @@
+package org.camp.mojiretsu;
+
+
+class name {
+   
+   public static void main(String[] ages){
+   
+     String word = "新井佳奈";
+   
+     int a = word.getBytes().length;
+     int b = word.length();
+     
+     System.out.println(a);
+     System.out.println(b); 
+   
+   } 
+}
