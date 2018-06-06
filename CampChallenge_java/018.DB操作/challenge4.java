fb465e8719cba3d8a630d788ab3b7548c4fc4623
araitsu1262 <brkn15005@g.nihon-u.ac.jp>
2018-06-06 20:21:06 +0900

    課題：特定のレコードの情報取得と表示

---
+++ CampChallenge_java/018.DB操作/challenge4.java
@@ -0,0 +1,52 @@
+package org.camp.db;
+
+
+import java.sql.*;
+
+public class challenge4 {
+    
+    public static void main(String[] ages){
+    
+    Connection db_con = null;
+    PreparedStatement db_st = null;
+    ResultSet db_data = null;
+    
+       try{
+
+          Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+          db_con = DriverManager.getConnection
+           ("jdbc:mysql://localhost:3306/Challenge_db"
+                  + "?characterEncoding=UTF-8&serverTimezone=JST","root","");
+       
+          db_st = db_con.prepareStatement("select * from profiles where profilesID=?");
+          db_st.setInt(1,1);
+          
+          db_data = db_st.executeQuery();
+          
+          while(db_data.next()){
+             System.out.println(db_data.getInt("profilesID")+" "
+                     +db_data.getString("name")+" "+db_data.getString("tel")+" "
+                     +db_data.getInt("age")+" "+db_data.getDate("birthday"));
+          }   
+          db_data.close();
+          db_st.close();
+          db_con.close();
+       
+       }catch(SQLException e_sql){
+           
+          System.out.println("接続時にエラーが発生しました"+e_sql.toString());       
+       
+       }catch(Exception e){
+       
+       }finally{
+       
+          if (db_con != null){
+             try {
+               db_con.close();
+             } catch (Exception e_con) {
+               System.out.println(e_con.getMessage());
+             }
+          }
+       }   
+    }               
+}
